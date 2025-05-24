760dbb1e4e334c9464d52d7313f8d4453fe41703
suriyaravichandran <rsuriya119@gmail.com>
2025-05-24 12:07:43 +0530

    url prames

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path("",include("myapps.urls")),
+    path("myapps/",include("myapps.urls")),
+    path("sales/",include("sales.urls")),
     path('admin/', admin.site.urls),
 ]
--- demo/myapps/urls.py
+++ demo/myapps/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
+
+app_name= 'myapps'
 urlpatterns = [
    path("",views.index,name="index"),
+   path("old_url",views.old_url,name="old_url"),
+   path("new_url",views.new_url,name="new_url"),
+   path("productdetail/<str:product_id>",views.productdetails,name="productdetail")
 ]
