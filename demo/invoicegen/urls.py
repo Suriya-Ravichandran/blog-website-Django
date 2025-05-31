6c81f727564347876bd09e1c53439d719c0ee997
suriyaravichandran <rsuriya119@gmail.com>
2025-05-31 11:41:29 +0530

    blog app creation

--- demo/invoicegen/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name= 'invoicegen'
-urlpatterns = [
-
-    path("invoice/pdf/",views.generate_pdf_bill,name="generate_pdf_bill"),
-    path("invoice/preview/",views.preview_invoice,name="preview_invoice"),
-]
