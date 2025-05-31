f5cbef504ef7be0ca4defbef4d98ead671f71fa4
suriyaravichandran <rsuriya119@gmail.com>
2025-05-31 13:15:50 +0530

    compiler add and create posts

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 app_name='blogapp'
 
 urlpatterns = [
-    path("",views.index,name="index")
+    path("",views.index,name="index"),
+    path("python",views.python,name="python")
 ]
--- blogapp/views.py
+++ blogapp/views.py
@@ -3,5 +3,24 @@ from django.http import HttpResponse
 # Create your views here.
 
 
+##### Demo codes here #####
+
+# def index(request):
+#     return HttpResponse("Hello world")
+
+
+#### Main code here ####
+
 def index(request):
-    return HttpResponse("Hello world")
+    return render(request,"index.html")
+
+def python(request):
+
+    posts=[
+        {"id":1,"title":"Python introduction","Content":"Demo content 1"},
+        {"id":2,"title":"Python Variables","Content":"Demo content 2"},
+        {"id":3,"title":"Python Operators","Content":"Demo content 3"},
+    ]
+
+
+    return render(request,"posts.html",{"posts": posts})
