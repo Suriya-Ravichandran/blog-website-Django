3347c620499aeb76e120ce5080fa2ccaec0581af
suriyaravichandran <rsuriya119@gmail.com>
2025-06-12 20:30:38 +0530

    intial commit

---
+++ blog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    return render(request,"index.html")
+
+def details(request,id):
+    return render(request,"details.html")
+
--- blogapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-app_name='blogapp'
-
-urlpatterns = [
-    path("",views.index,name="index"),
-    path("python",views.python,name="python"),
-    path("postdetials/<str:id>",views.postdetails,name="postdetails")
-]
