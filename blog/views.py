099303b4e065118af58754cdcc9651c3ccb23606
suriyaravichandran <rsuriya119@gmail.com>
2025-06-16 20:27:21 +0530

    update

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 from . models import Post
+from django.core.paginator import Paginator
 # Create your views here.
 def index(request):
     blog_title="Latest Post"
     allpost=Post.objects.all()
-    return render(request,"index.html",{'blog_title':blog_title,'allpost':allpost})
+
+    pagenator=Paginator(allpost,2)
+    page_number=request.GET.get("page")
+    page_object=pagenator.get_page(page_number)
+
+
+    return render(request,"index.html",{'blog_title':blog_title,'allpost':allpost,"page_obj":page_object})
+
 
 def details(request,slug):
-    return render(request,"details.html")
+    try:
+        post=Post.objects.get(slug=slug)
+        relatedpost=Post.objects.filter(category=post.category).exclude(pk=post.id)
+    except Post.DoesNotExist:
+        raise Http404("Post does not exits")
+    return render(request,"details.html",{"post":post,"relatedpost":relatedpost})
 
